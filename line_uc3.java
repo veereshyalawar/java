2648d91312cc0e70c337c541fbcd7be4c031e3ea
veeresh <veereshyalawar2@gmail.com>
2022-02-15 09:40:25 -0800

    added

---
+++ line_uc3.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class line_uc3 {
+
+	public static void main(String[] args) {
+
+	    Scanner.Scanner = new Scanner(System.in);
+		
+		System.out.println("enter x1");
+		int x1 = Scanner.nextInt();
+		
+		System.out.println("enter y1");
+		int y1 = Scanner.nextInt();
+		
+		System.out.println("enter x2");
+		int x2 = Scanner.nextInt();
+		
+		System.out.println("enter y2");
+		int y2 = Scanner.nextInt();
+		
+		int val1=(int) Math.pow((x2-x1),2);
+		int val2=(int) Math.pow((y2-y1),2);
+		int result=(int) Math.sqrt(val1+val2);
+	
+		    return result;
+	}
+
+	public static void main(string[] args) {
+
+	    Scanner.Scanner = new Scanner(System.in);
+		
+		System.out.println("enter values for line 1");
+		int lengthofline_1 = getLengthofLine(Scanner);
+		
+		System.out.println("enter values for line 1");
+		int lengthofline_1 = getLengthofLine(Scanner);
+		
+		String val1=String.valueOf("lengthofline_1");
+		String val1=String.valueOf("lengthofline_2");
+		
+		if(val1 >= val2)
+			System.out.println("first line is greater than second line ");
+		else
+			System.out.println("second line is greater than first line ");
+	}
+	scanner.close()
+}
