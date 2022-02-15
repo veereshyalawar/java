357ff60794d1bfd61f3a9fe19ebd2f3b87310533
veeresh <veereshyalawar2@gmail.com>
2022-02-15 09:38:39 -0800

    added line uc2

---
+++ line_uc_2.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class line_uc_2 {
+
+	public static void main(String[] args) {
+	
+
+	    Scanner.Scanner = new Scanner(System.in);
+		
+		System.out.println("enter x1");
+		int x1 = Scanner.nextInt();
+		
+		System.out.println("enter y1");
+		int y1 = Scanner.nextInt();
+		
+		System.out.println("enter x2");
+		int x2 = Scanner.nextInt();
+		
+		System.out.println("enter y2");
+		int y2 = Scanner.nextInt();
+		
+		int val1=(int) Math.pow((x2-x1),2);
+		int val2=(int) Math.pow((y2-y1),2);
+		int result=(int) Math.sqrt(val1+val2);
+	
+		    return result;
+	}
+
+	public static void main(string[] args) {
+
+	    Scanner.Scanner = new Scanner(System.in);
+		
+		System.out.println("enter values for line 1");
+		int lengthofline_1 = getLengthofLine(Scanner);
+		
+		System.out.println("enter values for line 1");
+		int lengthofline_1 = getLengthofLine(Scanner);
+		
+		String val1=String.valueOf("lengthofline_1");
+		String val1=String.valueOf("lengthofline_2");
+		
+		if(val1.equals(val2)) {
+			System.out.println("both lines are equal");
+		}
+		else {
+			System.out.println("both lines are not equal");
+		}
+		scanner.close()
+	}
+}
+	
+	
+	
+	
+	
