b5223bf3dcb3dc9864c7efbfd95f13d34f61497d
veeresh <veereshyalawar2@gmail.com>
2022-02-15 09:26:11 -0800

    added uc4

---
+++ wages_switch_uc4.java
@@ -0,0 +1,29 @@
+
+public class wages_switch_uc4 {
+	public static void main(String[] args) {
+		
+	       int Is_Full_Time = 1;
+	       int empCheck;
+	      
+	       double empCheck = Math.floor(Math.random() * 10) % 2;
+			 
+			
+	    	
+	    	
+	       switch (empCheck) {
+	       case 0:
+	           System.out.println("employ is present");
+	           break;
+	       case 1:
+	    	   System.out.println("employ is not present");
+	           break;
+	       default:
+	    	   System.out.println("enter valid");
+	    	   break;
+	       }
+	
+	
+	
+	}
+	       
+}
