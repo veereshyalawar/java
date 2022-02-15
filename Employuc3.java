7bfee118e6f5cd1097afe7c9194206358a7792e3
veeresh <veereshyalawar2@gmail.com>
2022-02-15 09:24:03 -0800

    added u 3

---
+++ Employuc3.java
@@ -0,0 +1,20 @@
+
+public class Employuc3 {
+
+	public static void main(String[] args) {
+	
+       int Is_Part_Time = 1;
+       int EmpHrs = 8;
+       int emp_wages_pHr = 20;
+       int emp_daily_wage = 0;
+      
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+		 
+	    if (empCheck == Is_Part_Time);
+    	
+    	emp_daily_wage = (EmpHrs * emp_wages_pHr);
+            
+    	System.out.println("employ daily salary :" + emp_daily_wage );
+          
+          }
+}
