bc202d5c2db11e056cae40b7506ad76499a35670
veeresh <veereshyalawar2@gmail.com>
2022-02-15 09:21:24 -0800

    added uc2

---
+++ employdailysalary.java
@@ -0,0 +1,19 @@
+
+public class employdailysalary {
+	
+	public static void main(String[] args) {
+	
+       int Is_Full_Time = 1;
+       int EmpHrs = 8;
+       int emp_wages_pHr = 20;
+       int emp_daily_wage = 0;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+		 
+    	emp_daily_wage = (EmpHrs * emp_wages_pHr);
+    	if (empCheck == Is_Full_Time);
+    	emp_daily_wage = (EmpHrs * emp_wages_pHr);
+    	       System.out.println("employ daily salary :" + emp_daily_wage);
+    	     
+    	    	   
+		}
+}
